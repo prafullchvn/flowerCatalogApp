7c2eab1db7badf82fcff5f56e6dd8201589ec83d
Prafull Chavan <99485204+prafullchvn@users.noreply.github.com>
2022-06-30 15:01:03 +0530

    Uninstalled server implemented using "net".

--- index.js
+++ index.js
@@ -2,5 +2,4 @@ const { router } = require('./src/router/routes.js');
 const { startServer } = require('server-using-http-module');
 
 const PORT = 80;
-const rootDir = process.argv[2] || './public';
-startServer(PORT, router, rootDir);
+startServer(PORT, router);
